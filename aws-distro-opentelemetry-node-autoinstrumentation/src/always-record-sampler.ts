48c2f913fa1c2f4519eb1fdc1b909799a69dca72
jjllee <jjllee@amazon.com>
2024-07-31 09:23:25 -0700

    AppSignals Functionality - add AlwaysRecordSampler and Utility files

---
+++ aws-distro-opentelemetry-node-autoinstrumentation/src/always-record-sampler.ts
@@ -0,0 +1,67 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { Context, Link, SpanAttributes, SpanKind } from '@opentelemetry/api';
+import { Sampler, SamplingDecision, SamplingResult } from '@opentelemetry/sdk-trace-base';
+
+/**
+ * This sampler will return the sampling result of the provided {@link #rootSampler}, unless the
+ * sampling result contains the sampling decision {@link SamplingDecision.NOT_RECORD}, in which case, a
+ * new sampling result will be returned that is functionally equivalent to the original, except that
+ * it contains the sampling decision {@link SamplingDecision.RECORD}. This ensures that all
+ * spans are recorded, with no change to sampling.
+ *
+ * <p>The intended use case of this sampler is to provide a means of sending all spans to a
+ * processor without having an impact on the sampling rate. This may be desirable if a user wishes
+ * to count or otherwise measure all spans produced in a service, without incurring the cost of 100%
+ * sampling.
+ */
+export class AlwaysRecordSampler implements Sampler {
+  private rootSampler: Sampler;
+
+  public static create(rootSampler: Sampler): AlwaysRecordSampler {
+    return new AlwaysRecordSampler(rootSampler);
+  }
+
+  private constructor(rootSampler: Sampler) {
+    if (rootSampler === null) {
+      throw new Error('rootSampler is null. It must be provided');
+    }
+    this.rootSampler = rootSampler;
+  }
+
+  shouldSample(
+    context: Context,
+    traceId: string,
+    spanName: string,
+    spanKind: SpanKind,
+    attributes: SpanAttributes,
+    links: Link[]
+  ): SamplingResult {
+    const rootSamplerSamplingResult: SamplingResult = this.rootSampler.shouldSample(
+      context,
+      traceId,
+      spanName,
+      spanKind,
+      attributes,
+      links
+    );
+    if (rootSamplerSamplingResult.decision === SamplingDecision.NOT_RECORD) {
+      return this.wrapResultWithRecordOnlyResult(rootSamplerSamplingResult);
+    }
+    return rootSamplerSamplingResult;
+  }
+
+  toString(): string {
+    return `AlwaysRecordSampler{${this.rootSampler.toString()}}`;
+  }
+
+  wrapResultWithRecordOnlyResult(result: SamplingResult) {
+    const wrappedResult: SamplingResult = {
+      decision: SamplingDecision.RECORD,
+      attributes: result.attributes,
+      traceState: result.traceState,
+    };
+    return wrappedResult;
+  }
+}
---
+++ aws-distro-opentelemetry-node-autoinstrumentation/src/aws-attribute-keys.ts
@@ -0,0 +1,31 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+export const AWS_ATTRIBUTE_KEYS: { [key: string]: string } = {
+  AWS_SPAN_KIND: 'aws.span.kind',
+  AWS_LOCAL_SERVICE: 'aws.local.service',
+  AWS_LOCAL_OPERATION: 'aws.local.operation',
+  AWS_REMOTE_SERVICE: 'aws.remote.service',
+  AWS_REMOTE_OPERATION: 'aws.remote.operation',
+  AWS_REMOTE_RESOURCE_TYPE: 'aws.remote.resource.type',
+  AWS_REMOTE_RESOURCE_IDENTIFIER: 'aws.remote.resource.identifier',
+  AWS_SDK_DESCENDANT: 'aws.sdk.descendant',
+  AWS_CONSUMER_PARENT_SPAN_KIND: 'aws.consumer.parent.span.kind',
+
+  AWS_REMOTE_TARGET: 'aws.remote.target',
+  AWS_REMOTE_DB_USER: 'aws.remote.db.user',
+
+  // Used for JavaScript workaround - attribute for pre-calculated value of isLocalRoot
+  APPSIGNALS_IS_LOCAL_ROOT: 'appsignals.is.local.root',
+
+  // Divergence from Java/Python
+  // TODO: Audit this: These will most definitely be different in JavaScript.
+  //   For example:
+  //     - `messaging.url` for AWS_QUEUE_URL
+  //     - `aws.dynamodb.table_names` for AWS_TABLE_NAME
+  AWS_BUCKET_NAME: 'aws.bucket.name',
+  AWS_QUEUE_URL: 'aws.queue.url',
+  AWS_QUEUE_NAME: 'aws.queue.name',
+  AWS_STREAM_NAME: 'aws.stream.name',
+  AWS_TABLE_NAME: 'aws.table.name',
+};

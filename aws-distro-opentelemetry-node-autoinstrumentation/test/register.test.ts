4dee0b2a0c118b6a09aeae4b44eaf8d47b952e71
jjllee <jjllee@amazon.com>
2024-08-14 10:47:36 -0700

    remove duplicate test in register.test.ts

--- aws-distro-opentelemetry-node-autoinstrumentation/test/register.test.ts
+++ aws-distro-opentelemetry-node-autoinstrumentation/test/register.test.ts
@@ -6,6 +6,7 @@ import * as assert from 'assert';
 import { spawnSync, SpawnSyncReturns } from 'child_process';
 
 // The OpenTelemetry Authors code
+// Extend register.test.ts functionality to also test exported span with Application Signals enabled
 describe('Register', function () {
   it('can load auto instrumentation from command line', () => {
     const proc: SpawnSyncReturns<Buffer> = spawnSync(
@@ -79,90 +80,4 @@ describe('Register', function () {
       'console span output in stdout - validate aws.span.kind'
     );
   });
-
-  it('can load Application Signals auto instrumentation from command line with instrumentation patching applied', () => {
-    const proc: SpawnSyncReturns<Buffer> = spawnSync(
-      process.execPath,
-      ['--require', '../build/src/register.js', './test-app/app-aws-sdk-s3-call.js'],
-      {
-        cwd: __dirname,
-        timeout: 10000,
-        killSignal: 'SIGKILL', // SIGTERM is not sufficient to terminate some hangs
-        env: Object.assign({}, process.env, {
-          OTEL_NODE_RESOURCE_DETECTORS: 'none',
-          OTEL_TRACES_EXPORTER: 'console',
-          // nx (used by lerna run) defaults `FORCE_COLOR=true`, which in
-          // node v18.17.0, v20.3.0 and later results in ANSI color escapes
-          // in the ConsoleSpanExporter output that is checked below.
-          FORCE_COLOR: '0',
-
-          OTEL_LOG_LEVEL: 'ALL',
-          OTEL_TRACES_SAMPLER: 'always_on',
-          OTEL_EXPORTER_OTLP_TRACES_ENDPOINT: 'http://localhost:4316/v1/traces',
-          OTEL_RESOURCE_ATTRIBUTES: 'service.name=test-adot-sdk-ec2-service-name',
-          OTEL_AWS_APPLICATION_SIGNALS_ENABLED: 'true',
-          OTEL_NODE_DISABLED_INSTRUMENTATIONS: 'fs',
-        }),
-      }
-    );
-    assert.ifError(proc.error);
-    assert.equal(proc.status, 0, `proc.status (${proc.status})`);
-    assert.equal(proc.signal, null, `proc.signal (${proc.signal})`);
-
-    assert.ok(proc.stdout.includes('AWS Distro of OpenTelemetry automatic instrumentation started successfully'));
-    assert.ok(proc.stdout.includes("Environment variable OTEL_EXPORTER_OTLP_PROTOCOL is set to 'http/protobuf'"));
-    assert.ok(proc.stdout.includes("Environment variable OTEL_PROPAGATORS is set to 'xray,tracecontext,b3,b3multi'"));
-
-    // Check a span has been generated for the GET request done in app.js
-    assert.ok(proc.stdout.includes("name: 'GET'"), 'console span output in stdout - validate Span Name');
-    assert.ok(
-      proc.stdout.includes("'service.name': 'test-adot-sdk-ec2-service-name'"),
-      'console span output in stdout - validate service.name'
-    );
-
-    // eslint-disable-next-line @typescript-eslint/typedef
-    const packageJson = require('./../../package.json');
-    const DISTRO_VERSION: string = packageJson.version;
-    assert.ok(
-      proc.stdout.includes(`'telemetry.auto.version': '${DISTRO_VERSION}-aws'`),
-      'console span output in stdout - validate telemetry.auto.version'
-    );
-    assert.ok(
-      proc.stdout.includes("'aws.is.local.root': true"),
-      'console span output in stdout - validate aws.is.local.root'
-    );
-    assert.ok(
-      proc.stdout.includes("'aws.local.operation': 'InternalOperation'"),
-      'console span output in stdout - validate aws.local.operation'
-    );
-    assert.ok(
-      proc.stdout.includes("'aws.local.service': 'test-adot-sdk-ec2-service-name'"),
-      'console span output in stdout - validate aws.local.service'
-    );
-    assert.ok(
-      proc.stdout.includes("'aws.remote.service': 'AWS::S3'"),
-      'console span output in stdout - validate aws.remote.service'
-    );
-    assert.ok(
-      proc.stdout.includes("'aws.remote.operation': 'ListObjects'"),
-      'console span output in stdout - validate aws.remote.operation'
-    );
-    assert.ok(
-      proc.stdout.includes("'aws.span.kind': 'LOCAL_ROOT'"),
-      'console span output in stdout - validate aws.span.kind'
-    );
-    assert.ok(
-      proc.stdout.includes("'aws.s3.bucket': 'test-bucket-not-exists'"),
-      'console span output in stdout - validate aws.s3.bucket'
-    );
-    assert.ok(
-      proc.stdout.includes("'aws.remote.resource.type': 'AWS::S3::Bucket'"),
-      'console span output in stdout - validate aws.remote.resource.type'
-    );
-    assert.ok(
-      proc.stdout.includes("'aws.remote.resource.identifier': 'test-bucket-not-exists'"),
-      'console span output in stdout - validate aws.remote.resource.identifier'
-    );
-  });
 });
-// END The OpenTelemetry Authors code

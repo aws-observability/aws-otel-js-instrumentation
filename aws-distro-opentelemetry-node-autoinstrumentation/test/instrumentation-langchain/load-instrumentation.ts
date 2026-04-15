7850ac9c5ed8ec997163c6618e95a7bcd828704a
Steve Liu <liustve@amazon.com>
2026-04-15 16:47:05 -0700

    Add missing load-instrumentation.ts test file

---
+++ aws-distro-opentelemetry-node-autoinstrumentation/test/instrumentation-langchain/load-instrumentation.ts
@@ -0,0 +1,14 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { registerInstrumentationTesting } from '@opentelemetry/contrib-test-utils';
+import { LangChainInstrumentation } from '../../src/instrumentation/instrumentation-langchain';
+
+export const instrumentation = new LangChainInstrumentation();
+registerInstrumentationTesting(instrumentation);
+instrumentation.disable();
+
+export const contentCaptureInstrumentation = new LangChainInstrumentation({
+  captureMessageContent: true,
+});
+contentCaptureInstrumentation.disable();

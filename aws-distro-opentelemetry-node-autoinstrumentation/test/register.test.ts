6c26c55a55d0c35bd522f1dfb32bd4e181bc545b
Steve Liu <liustve@amazon.com>
2026-04-17 18:47:32 -0700

    Stub NodeSDK.start in register.test.ts to prevent duplicate provider registration

--- aws-distro-opentelemetry-node-autoinstrumentation/test/register.test.ts
+++ aws-distro-opentelemetry-node-autoinstrumentation/test/register.test.ts
@@ -5,6 +5,8 @@
 import * as assert from 'assert';
 import { spawnSync, SpawnSyncReturns } from 'child_process';
 import expect from 'expect';
+import * as opentelemetry from '@opentelemetry/sdk-node';
+import * as sinon from 'sinon';
 import { LangChainInstrumentation } from '../src/instrumentation/instrumentation-langchain/instrumentation';
 
 // The OpenTelemetry Authors code
@@ -17,7 +19,9 @@ describe('Register', function () {
     // Lazy-load register.ts inside before() to avoid polluting the process with
     // require-in-the-middle hooks at file parse time. This prevents interference
     // with other instrumentation tests that run in the same mocha process.
+    const stub = sinon.stub(opentelemetry.NodeSDK.prototype, 'start');
     const register = require('../src/register');
+    stub.restore();
     instrumentations = register.instrumentations;
     setAwsDefaultEnvironmentVariables = register.setAwsDefaultEnvironmentVariables;
   });

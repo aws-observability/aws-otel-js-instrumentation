cd77ce5d3ffc7cb27183ae6dc5229242c8dbc9c8
Jonathan Lee <jjllee2022@hotmail.com>
2026-06-17 18:46:48 -0700

    checkout lambda-layer of 75c016d5eefef9edcefd6c077386be8657b07197

--- lambda-layer/sample-apps/aws-sdk/src/index.ts
+++ lambda-layer/sample-apps/aws-sdk/src/index.ts
@@ -5,30 +5,16 @@ import {
 } from 'aws-lambda';
 
 import { S3 } from '@aws-sdk/client-s3';
-import winston from 'winston';
-import { OpenTelemetryTransportV3 } from '@opentelemetry/winston-transport';
 
 const s3 = new S3();
-const logger = winston.createLogger({
-  level: 'debug',
-  transports: [
-    new winston.transports.Console(),
-    new OpenTelemetryTransportV3(),
-  ],
-});
 
 exports.handler = async (_event: APIGatewayProxyEvent, _context: Context) => {
-  logger.debug('debug-level-test-message');
-  logger.info('info-level-test-message');
-  logger.warn('warn-level-test-message');
-  logger.error('error-level-test-message');
-
-  logger.info('Serving lambda request.');
+  console.info('Serving lambda request.');
 
   const result = await s3.listBuckets();
 
-  logger.info('Fetched OTel Resource Attrs:' + process.env.OTEL_RESOURCE_ATTRIBUTES);
-  logger.info('Fetched X-Ray Trace Header:' + process.env['_X_AMZN_TRACE_ID']);
+  console.log('Fetched OTel Resource Attrs:' + process.env.OTEL_RESOURCE_ATTRIBUTES);
+  console.log('Fetched X-Ray Trace Header:' + process.env['_X_AMZN_TRACE_ID']);
 
   const response: APIGatewayProxyResult = {
     statusCode: 200,
